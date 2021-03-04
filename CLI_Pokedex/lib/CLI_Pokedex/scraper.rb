f2631d48fed0fa3f505f375fae512787ba1b8996
Thor Summerall <thor.summerall@gmail.com>
2021-03-04 14:17:49 +0000

    working on scraper now, and fixed the attr_accessor for Pokemon class

--- CLI_Pokedex/lib/CLI_Pokedex/scraper.rb
+++ CLI_Pokedex/lib/CLI_Pokedex/scraper.rb
@@ -3,9 +3,8 @@ class CLIPokedex::Scraper
   ## all pokemon names have pokemon in their links. 
   def get_page
     doc = Nokogiri::HTML(open("https://bulbapedia.bulbagarden.net/wiki/List_of_Pokemon_by_National_Pokedex_number"))
-    
-    grabbing_table = doc.css("td a[href]")
-    
-    puts grabbing_table
+    #grabs all links from the page 
+    pokemon_list = doc.css("td a")
   end
+  
 end 
