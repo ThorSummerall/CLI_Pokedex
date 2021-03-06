f93796f2f911b8ba81e7664295c70ce7e5fcf216
Thor Summerall <thor.summerall@gmail.com>
2021-03-06 14:18:58 +0000

    still messing around with getting into links

--- CLI_Pokedex/lib/CLI_Pokedex/scraper.rb
+++ CLI_Pokedex/lib/CLI_Pokedex/scraper.rb
@@ -1,16 +1,19 @@
 class CLIPokedex::Scraper
 
-  ## all pokemon names have pokemon in their links.
-  def get_page
+  def get_pokemon_links
     doc = Nokogiri::HTML(open("https://bulbapedia.bulbagarden.net/wiki/List_of_Pokemon_by_National_Pokedex_number"))
     #puts all links from the page
     list_of_pokemon = doc.css("td a").to_s.split
-
     list_of_pokemon.each do |x|
       if x.include? "(Pok%C3%A9mon)"
-        wiki_link = x.delete "href="
-        puts wiki_link
+        @@wiki_link = x[6...-1]
       end
     end
+  end
+
+  def get_pokemon_name(wiki_link)
+    url = "https://bulbapedia.bulbagarden.net#{wiki_link}"
+    html = open(url)
+    docx = Nokogiri::HTML(html)
   end
 end
