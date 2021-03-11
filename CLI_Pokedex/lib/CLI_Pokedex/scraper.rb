d90893a9bf941dce15640193d812862726f4c5a4
Thor Summerall <thor.summerall@gmail.com>
2021-03-11 15:16:10 +0000

    properly scrapes the page of individual pokemon

--- CLI_Pokedex/lib/CLI_Pokedex/scraper.rb
+++ CLI_Pokedex/lib/CLI_Pokedex/scraper.rb
@@ -1,5 +1,7 @@
 class CLIPokedex::Scraper
 
+  @@wiki_link = []
+
   def get_pokemon_links
     doc = Nokogiri::HTML(open("https://bulbapedia.bulbagarden.net/wiki/List_of_Pokemon_by_National_Pokedex_number"))
     #puts all links from the page
@@ -11,9 +13,11 @@ class CLIPokedex::Scraper
     end
   end
 
-  def get_pokemon_name(wiki_link)
-    url = "https://bulbapedia.bulbagarden.net#{wiki_link}"
+  def get_pokemon_name
+    url = "https://bulbapedia.bulbagarden.net#{@@wiki_link}"
     html = open(url)
     docx = Nokogiri::HTML(html)
+
+    puts docx
   end
 end
